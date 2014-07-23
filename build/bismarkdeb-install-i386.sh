d8b69614a77003dc8753ec9bd89f82f74e8650da
gmartins <ggmartins@gmail.com>
2014-07-23 21:16:46 +0000

    time cmd for installs sh

--- build/bismarkdeb-install-i386.sh
+++ build/bismarkdeb-install-i386.sh
@@ -4,7 +4,7 @@ wget -c http://downloads.projectbismark.net/bismark-active_1.0-1_i386.deb
 wget -c http://downloads.projectbismark.net/bismark-data-transmit_1.0-1_i386.deb
 wget -c http://downloads.projectbismark.net/bismark-management-client_1.0-1_i386.deb
 wget -c http://downloads.projectbismark.net/bismark-netcat-gnu_0.7.1-1_i386.deb
-apt-get install netperf paris-traceroute d-itg curl libcurl3-gnutls fping dnsutils
+apt-get install netperf paris-traceroute d-itg curl libcurl3-gnutls fping dnsutils time
 dpkg -i bismark-management-client_1.0-1_i386.deb
 dpkg -i bismark-data-transmit_1.0-1_i386.deb
 dpkg -i bismark-active_1.0-1_i386.deb
