e835501ad72e3985b1e5bd96b552cb9e6a06155c
gmartins <ggmartins@gmail.com>
2014-08-08 16:09:34 +0000

    toolid summary

--- test/verifyxmls.sh
+++ test/verifyxmls.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 URL=http://uploads.projectbismark.net/DLB827EB181362/20140718/
-
+URL=http://uploads.projectbismark.net/OW4C60DEE6C732/20140722/
 wget $URL
 
 cat index.html | grep -o "active.*gz\"" | sed "s/gz\"/gz/g" > tgzlist
@@ -14,3 +14,6 @@ cd $(echo $f | sed "s/.tar.gz//g"); mv *.xml ..; cd -
 rmdir $(echo $f | sed "s/.tar.gz//g") 
 rm $f
 done
+rm index.html
+cat *.xml | grep -o "tool\(id\)\?=\"[a-zA-Z0-9_]\+\"" | sort | uniq -c
+
