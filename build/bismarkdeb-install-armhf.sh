f6a7757c2c272282eab019aeb86544149f36fea2
gmartins <ggmartins@gmail.com>
2014-08-21 12:35:05 +0000

    install script link fixes

--- build/bismarkdeb-install-armhf.sh
+++ build/bismarkdeb-install-armhf.sh
@@ -2,13 +2,13 @@
 
 apt-get update
 
-#wget -c http://guilherme.noise.gatech.edu:8000/bismark-active_1.0-1_armhf.deb
-#wget -c http://guilherme.noise.gatech.edu:8000/bismark-data-transmit_1.0-1_armhf.deb
-#wget -c http://guilherme.noise.gatech.edu:8000/bismark-management-client_1.0-1_armhf.deb
-#wget -c http://guilherme.noise.gatech.edu:8000/bismark-netcat-gnu_0.7.1-1_armhf.deb
-#wget -c http://guilherme.noise.gatech.edu:8000/bismark-dropbear_2011.54-1_armhf.deb
-#wget -c http://guilherme.noise.gatech.edu:8000/bismark-shaperprobe_2009.10_armhf.deb
-#wget -c http://guilherme.noise.gatech.edu:8000/bismark-netperf_2.4.4-1_armhf.deb
+wget -c http://downloads.projectbismark.net/armhf/bismark-active_1.0-1_armhf.deb
+wget -c http://downloads.projectbismark.net/armhf/bismark-data-transmit_1.0-1_armhf.deb
+wget -c http://downloads.projectbismark.net/armhf/bismark-management-client_1.0-1_armhf.deb
+wget -c http://downloads.projectbismark.net/armhf/bismark-netcat-gnu_0.7.1-1_armhf.deb
+wget -c http://downloads.projectbismark.net/armhf/bismark-dropbear_2011.54-1_armhf.deb
+wget -c http://downloads.projectbismark.net/armhf/bismark-shaperprobe_2009.10_armhf.deb
+wget -c http://downloads.projectbismark.net/armhf/bismark-netperf_2.4.4-1_armhf.deb
 apt-get -y install netperf paris-traceroute d-itg curl libcurl3-gnutls fping dnsutils time
 dpkg -i bismark-netcat-gnu_0.7.1-1_armhf.deb
 dpkg -i bismark-dropbear_2011.54-1_armhf.deb
--- build/bismarkdeb-install-i386.sh
+++ build/bismarkdeb-install-i386.sh
@@ -2,13 +2,13 @@
 
 apt-get update
 
-#wget -c http://guilherme.noise.gatech.edu:8000/bismark-active_1.0-1_i386.deb
-#wget -c http://guilherme.noise.gatech.edu:8000/bismark-data-transmit_1.0-1_i386.deb
-#wget -c http://guilherme.noise.gatech.edu:8000/bismark-management-client_1.0-1_i386.deb
-#wget -c http://guilherme.noise.gatech.edu:8000/bismark-netcat-gnu_0.7.1-1_i386.deb
-#wget -c http://guilherme.noise.gatech.edu:8000/bismark-dropbear_2011.54-1_i386.deb
-#wget -c http://guilherme.noise.gatech.edu:8000/bismark-shaperprobe_2009.10_i386.deb
-#wget -c http://guilherme.noise.gatech.edu:8000/bismark-netperf_2.4.4-1_i386.deb
+wget -c http://downloads.projectbismark.net/i386/bismark-active_1.0-1_i386.deb
+wget -c http://downloads.projectbismark.net/i386/bismark-data-transmit_1.0-1_i386.deb
+wget -c http://downloads.projectbismark.net/i386/bismark-management-client_1.0-1_i386.deb
+wget -c http://downloads.projectbismark.net/i386/bismark-netcat-gnu_0.7.1-1_i386.deb
+wget -c http://downloads.projectbismark.net/i386/bismark-dropbear_2011.54-1_i386.deb
+wget -c http://downloads.projectbismark.net/i386/bismark-shaperprobe_2009.10_i386.deb
+wget -c http://downloads.projectbismark.net/i386/bismark-netperf_2.4.4-1_i386.deb
 apt-get -y install netperf paris-traceroute d-itg curl libcurl3-gnutls fping dnsutils time
 dpkg -i bismark-netcat-gnu_0.7.1-1_i386.deb
 dpkg -i bismark-dropbear_2011.54-1_i386.deb
