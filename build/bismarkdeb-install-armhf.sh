f8056db84e0401433c962445d307914af636642a
ggmartins@gmail.com <ggmartins@gmail.com>
2016-08-26 20:43:50 +0000

    dhcpd settings

--- build/bismarkdeb-install-armhf.sh
+++ build/bismarkdeb-install-armhf.sh
@@ -18,6 +18,29 @@ apt-get -yqf install paris-traceroute
 apt-get -yqf install d-itg
 apt-get -yqf install curl
 apt-get -yqf install libcurl3-gnutls
+apt-get -yqf install isc-dhcp-server 
+
+
+while true; do
+    read -p "Do you wish to apply the BISmark isc-dhcp-server settings?" yn
+    case $yn in
+        [Yy]* ) 
+                 if [ -f /etc/default/isc-dhcp-server ]; then
+                    echo -e "INTERFACES=\"eth1\"\n" >> /etc/default/isc-dhcp-server
+                 else
+                    echo "Warning: no default file for isc-dhcp-server" 
+                 fi
+                 if [ -f /etc/dhcp/dhcpd.conf ]; then
+                    echo "subnet 10.1.1.0 netmask 255.255.255.0 {range 10.1.1.10 10.1.1.100; option routers 10.1.1.1;}" >> /etc/dhcp/dhcpd.conf
+                 else
+                    echo "Warning: no default file for isc-dhcp-server" 
+                 fi
+              break;;
+        [Nn]* ) echo "OK";;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 dpkg -i bismark-netcat-gnu_0.7.1-1_armhf.deb
 dpkg -i bismark-dropbear_2011.54-1_armhf.deb
 dpkg -i bismark-management-client_1.0-1_armhf.deb
