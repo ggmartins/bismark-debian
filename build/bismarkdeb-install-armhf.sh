a225098331088f82622690f3bbcc9a52c640c367
ggmartins <ggmartins@gmail.com>
2016-08-29 15:23:43 +0000

    bismark_nat

--- build/bismarkdeb-install-armhf.sh
+++ build/bismarkdeb-install-armhf.sh
@@ -19,7 +19,7 @@ apt-get -yqf install d-itg
 apt-get -yqf install curl
 apt-get -yqf install libcurl3-gnutls
 apt-get -yqf install isc-dhcp-server 
-
+apt-get -yqf install dropbear
 
 while true; do
     read -p "Do you wish to apply the BISmark isc-dhcp-server settings?" yn
@@ -42,10 +42,48 @@ while true; do
 done
 
 dpkg -i bismark-netcat-gnu_0.7.1-1_armhf.deb
-dpkg -i bismark-dropbear_2011.54-1_armhf.deb
+#dpkg -i bismark-dropbear_2011.54-1_armhf.deb
 dpkg -i bismark-management-client_1.0-1_armhf.deb
 dpkg -i bismark-data-transmit_1.0-1_armhf.deb
 dpkg -i bismark-active_1.0-1_armhf.deb
 dpkg -i bismark-netperf_2.4.4-1_armhf.deb
 dpkg -i bismark-shaperprobe_2009.10_armhf.deb
+
+cat << "EOF" | tee /etc/init.d/bismark-nat > /dev/null
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          bismark-nat
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: bismark-nat
+# Description:       enable NAT for any traffic comming from eth0
+### END INIT INFO
+
+IFOUT="eth0"
+touch /var/lock/bismark-nat
+case "$1" in
+  start)
+    echo "Starting bismark-nat..."
+    modprobe iptable_nat
+    iptables -t nat -A POSTROUTING -o $IFOUT -j MASQUERADE
+    echo 1 > /proc/sys/net/ipv4/ip_forward
+    echo "Done."
+    ;;
+  stop)
+    echo "Stopping bismark-nat..."
+    echo 0 > /proc/sys/net/ipv4/ip_forward
+    echo "Done."
+    ;;
+  *)
+    echo "Usage: /etc/init.d/bismark-nat {start|stop}"
+    exit 1
+    ;;
+esac
+exit 0
+EOF
+
+chmod +x /etc/init.d/bismark-nat
+update-rc.d bismark-nat defaults
 
